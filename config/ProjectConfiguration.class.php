0770c46faa1504a1baf1284c6fb386ae9f4ed9f3
David Epely <esion99@gmail.com>
2009-03-22 15:58:53 +0100

    Begining of symfony freeze

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
+# FROZEN_SF_LIB_DIR: /usr/share/php/symfony
+
+require_once dirname(__FILE__).'/../lib/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
